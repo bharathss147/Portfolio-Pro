004fd33187fec02b26ceb753881dee49680758dc
Bharath <bharath.s.cse.2024@snsce.ac.in>
2026-07-07 00:32:47 +0530

    Configure custom domain bharathportfolio.me

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/Portfolio-Pro/",
+  base: "/",
   server: {
     host: "::",
     port: 3000,
